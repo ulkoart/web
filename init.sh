61a40c9ac7fe097fdbf0f84869c4069906561a2a
Artem Ulko <ulkoart@gmail.com>
2018-11-01 11:00:07 +0300

    add ask in  nginx.conf

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn_hello.conf   /etc/gunicorn.d/hello
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
