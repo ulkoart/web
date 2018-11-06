beb98f7962033f04d0e4d51ea7d7c30bdb6a320e
Artem Ulko <ulkoart@gmail.com>
2018-11-06 15:41:06 +0300

    Question model, Answer model

--- ask/qa/admin.py
+++ ask/qa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from qa.models import Question, Answer
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return super(QuestionManager, self).get_queryset().order_by('added_at')
+
+    def popular(self):
+        return super(QuestionManager, self).get_queryset().order_by('-likes')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
+    likes = models.ManyToManyField(User, blank=True)
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
