7070c9f40ad924fb38f484dd3757f506c22867c2
Artem Ulko <ulkoart@gmail.com>
2018-11-07 10:53:59 +0300

    add mysql sett

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -81,6 +81,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'aks',
+#         'USER': 'aks',
+#         'PASSWORD': 'aks',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# } 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
--- init.sh
+++ init.sh
@@ -4,3 +4,9 @@ sudo ln -s /home/box/web/etc/gunicorn_hello.conf   /etc/gunicorn.d/hello
 sudo ln -s /home/box/web/etc/gunicorn_ask.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
+
+mysql -uroot -e "CREATE DATABASE aks;"
+mysql -uroot -e "CREATE USER 'aks' IDENTIFIED BY 'aks';"
+mysql -uroot -e "GRANT ALL ON aks.* TO 'aks';"
+mysql -uroot -e "GRANT USAGE ON *.* TO 'aks';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
